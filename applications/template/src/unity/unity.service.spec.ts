5cecc2ae744906c6334e6d2aa6eb7b7559e4984c
naixatwork <naixatwork@gmail.com>
2022-08-15 20:56:30 +0430

    test(unity): introduced unit tests for unity.service.ts

---
+++ applications/template/src/unity/unity.service.spec.ts
@@ -0,0 +1,31 @@
+import {UnityService} from "./unity.service";
+import {createTestingModule} from "../shared/create-testing-module.function";
+import {UnityModule} from "./unity.module";
+import {IUnityInstance, UNITY_SERVICE_IDENTIFIERS} from "./unity.types";
+
+class UnityInstanceMock implements IUnityInstance {
+    public static hasBeenCalled = false;
+
+    SendMessage(gameObject: string, method: string, value: string): void {
+        UnityInstanceMock.hasBeenCalled = true;
+    }
+}
+
+describe("unity.service", () => {
+    let sut: UnityService;
+
+    beforeEach(() => {
+        const moduleRef = createTestingModule(UnityModule);
+        moduleRef.rebind<IUnityInstance>(UNITY_SERVICE_IDENTIFIERS.unityInstance).toConstantValue(new UnityInstanceMock())
+        sut = moduleRef.get(UnityService);
+    });
+
+    test("it should be defined", () => {
+        expect(sut).toBeDefined();
+    });
+
+    test("it should call UnityInstance's SendMessage method", () => {
+        sut.sendMessage("test", "test", "test");
+        expect(UnityInstanceMock.hasBeenCalled).toBeTruthy();
+    })
+});
--- applications/template/src/unity/unity.service.ts
+++ applications/template/src/unity/unity.service.ts
@@ -4,9 +4,7 @@ import {IUnityInstance, UNITY_SERVICE_IDENTIFIERS} from "./unity.types";
 
 @injectable()
 export class UnityService {
-    constructor(@inject(UNITY_SERVICE_IDENTIFIERS.unityInstance) private readonly unityInstance: IUnityInstance) {
-        console.log(unityInstance)
-    }
+    constructor(@inject(UNITY_SERVICE_IDENTIFIERS.unityInstance) private readonly unityInstance: IUnityInstance) {}
 
     public sendMessage(gameObject: string, method: string, value?: any): void {
         this.unityInstance.SendMessage(gameObject, method, value);
