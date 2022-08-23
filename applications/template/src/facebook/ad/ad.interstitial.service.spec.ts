975785550fff027342f50a6c8d198e7a21140267
naixatwork <naixatwork@gmail.com>
2022-08-23 13:20:33 +0430

    test(ad): introduced a test: it should send a message to unity if preloadAd() resolves successfully

--- applications/template/src/facebook/ad/ad.interstitial.service.spec.ts
+++ applications/template/src/facebook/ad/ad.interstitial.service.spec.ts
@@ -1,13 +1,32 @@
 import {AdInterstitialService} from "./ad.interstitial.service";
 import {createTestingModule} from "../../shared/create-testing-module.function";
-import {FacebookModule} from "../facebook.module";
+import {FacebookAdMock, FacebookModule} from "../facebook.module";
 import {UnityModule} from "../../unity/unity.module";
+import {IUnityInstance, PLANKTON_GAME_OBJECT_NAME, UNITY_SERVICE_IDENTIFIERS} from "../../unity/unity.types";
+
+class UnityInstanceMock implements IUnityInstance {
+    public static logStack: {
+        gameObject: string,
+        method: string,
+        value: string
+    }[] = [];
+
+    SendMessage(gameObject: string, method: string, value: string): void {
+        UnityInstanceMock.logStack.push({
+            gameObject,
+            method,
+            value
+        });
+        console.log(`[UnityInstanceMock]: ${gameObject} ${method} ${value}`);
+    }
+}
 
 describe("AdInterstitialService", () => {
     let sut: AdInterstitialService;
 
     beforeEach(() => {
         const moduleRef = createTestingModule(FacebookModule, UnityModule);
+        moduleRef.rebind(UNITY_SERVICE_IDENTIFIERS.unityInstance).toConstantValue(new UnityInstanceMock())
         sut = moduleRef.get(AdInterstitialService);
     });
 
@@ -15,8 +34,18 @@ describe("AdInterstitialService", () => {
         expect(sut).toBeDefined();
     });
 
-    test("it should return an Ad", async () => {
-        await sut.preloadAd("42323");
+    test("it should return store preloaded Ad instance after calling preloadAd()", async () => {
+        await sut.preloadAd("999");
         expect(sut.ad).toBeDefined();
+        expect(sut.ad).toBeInstanceOf(FacebookAdMock);
+    });
+
+    test("it should send a message to unity if preloadAd() resolves successfully", async () => {
+        await sut.preloadAd("999");
+        expect(UnityInstanceMock.logStack[0]).toEqual({
+            gameObject: PLANKTON_GAME_OBJECT_NAME,
+            method: "OnAdLoaded",
+            value: "interstitial"
+        })
     });
 });
